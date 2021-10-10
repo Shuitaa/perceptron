84add541b37003d99c763d35d7cf306d4373da43
Shuitaa <pierre.averty2001@gmail.com>
2021-10-10 11:26:35 +0200

    Implementing the pereptron function.

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ def main():
     learning_rate = 1
     bias = 1
     weights = [random.random(),random.random(),random.random()]
+
+
+def perceptron(in_a, in_b, out, learning_rate, bias, weights):
+    out_p = in_a * weights[0] + in_b * weights[1] + bias * weights[3]
+
+    if out_p > 0:
+        out_p = 1
+    else:
+        out_p = 0
+
+    err = out - out_p
+
+    weights[0] += err * in_a * learning_rate
+    weights[0] += err * in_b * learning_rate
+    weights[0] += err * bias * learning_rate
+
+    return out_p
     
 if __name__ == "__main__":
     main()
